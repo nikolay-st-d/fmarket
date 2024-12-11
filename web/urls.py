ad9825baf1f50ff2210d08f478bcc17161d64b82
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2024-12-11 13:35:36 +0200

    Added contact page form, views and templates

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from web.views import HomePageView
+from web.views import HomePageView, ContactView, SuccessView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('contact/success/', SuccessView.as_view(), name='contact-success'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mail
+from django.urls import reverse
 from django.views import generic as views
 from products.models import Product
+from web.forms import ContactForm
 
 
 class HomePageView(views.ListView):
@@ -10,3 +13,36 @@ class HomePageView(views.ListView):
     def get_queryset(self):
         products = Product.objects.all().order_by('-views_count', '-id')[:self.HOME_PAGE_LATEST_PRODUCTS_NUMBER]
         return products
+
+
+class SuccessView(views.TemplateView):
+    template_name = "contact-success.html"
+
+
+class ContactView(views.FormView):
+    form_class = ContactForm
+    template_name = "contact.html"
+
+    def get_success_url(self):
+        return reverse("contact-success")
+
+    def form_valid(self, form):
+        name = form.cleaned_data.get("name")
+        email = form.cleaned_data.get("email")
+        subject = form.cleaned_data.get("subject")
+        message = form.cleaned_data.get("message")
+
+        full_message = f"""
+            About: {subject}
+            From: {name} <{email}>
+            ________________________
+
+            {message}
+            """
+        send_mail(
+            subject="fMarket message",
+            message=full_message,
+            from_email=email,
+            recipient_list=['contact@ndimitrov.com'],
+        )
+        return super(ContactView, self).form_valid(form)
